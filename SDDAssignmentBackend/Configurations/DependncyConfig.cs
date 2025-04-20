f691cb1e248518814e8abe9601f18770a2f1d2a5
AmmarAllahham <ammar.lm96@gmail.com>
2025-04-20 11:58:43 +0400

    Refactor dependency configuration and add JWT options

--- SDDAssignmentBackend/Configurations/DependncyConfig.cs
+++ SDDAssignmentBackend/Configurations/DependncyConfig.cs
@@ -1,22 +1,47 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SDDAssignmentBackend.Configurations.Options;
 using SDDAssignmentBackend.Context;
+using SDDAssignmentBackend.Repositories.Implementation;
+using SDDAssignmentBackend.Repositories.Interface;
+using SDDAssignmentBackend.Services.Implementation;
+using SDDAssignmentBackend.Services.Interface;
 
 namespace SDDAssignmentBackend.Configurations
 {
     public static class DependncyConfig
     {
 
-        public static void AddDependencies(this IServiceCollection services, IConfiguration Configuration)
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
         {
-
-            services.UseSQLDB(Configuration);
+            services.AddOptions(configuration);
+            services.UseSQLDB(configuration);
+            services.AddRepositories();
+            services.AddServices();
         }
 
-        public static void UseSQLDB(this IServiceCollection services, IConfiguration Configuration)
+        public static void UseSQLDB(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
             );
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+        
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+
+        }
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        }
+        public static void AddOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtOption>(configuration.GetSection("Jwt"));
+
         }
     }
 }
---
+++ SDDAssignmentBackend/Configurations/Options/JwtOption.cs
@@ -0,0 +1,10 @@
+﻿namespace SDDAssignmentBackend.Configurations.Options
+{
+    public class JwtOption
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int Expired { get; set; }
+    }
+}
