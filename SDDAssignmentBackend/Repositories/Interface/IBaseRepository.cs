087a90c3b9b09fe8a7cfa0738122b0d9976dd52d
AmmarAllahham <ammar.lm96@gmail.com>
2025-04-20 11:55:43 +0400

    Add repository interfaces for entity management

---
+++ SDDAssignmentBackend/Repositories/Interface/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using SDDAssignmentBackend.Entities;
+
+namespace SDDAssignmentBackend.Repositories.Interface
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(Guid id);
+        Task<T?> GetByIdAsync(Guid id);
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+        IQueryable<T> AsQuerable();
+    }
+}
---
+++ SDDAssignmentBackend/Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using SDDAssignmentBackend.Entities;
+
+namespace SDDAssignmentBackend.Repositories.Interface
+{
+    public interface IUserRepository: IBaseRepository<UserEntity>
+    {
+        Task<UserEntity?> GetByUsernameAsync(string username);
+    }
+}
