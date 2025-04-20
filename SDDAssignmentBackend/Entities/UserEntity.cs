84e73dfdb1ae86345dd988ec28a6b329aba992c7
AmmarAllahham <ammar.lm96@gmail.com>
2025-04-20 11:55:11 +0400

    Add BaseEntity and UserEntity classes

---
+++ SDDAssignmentBackend/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SDDAssignmentBackend.Enums;
+
+namespace SDDAssignmentBackend.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        [MaxLength(100)]
+        public required string Username { get; set; }
+        [MaxLength(255)]
+        public required string PasswordHash { get; set; }
+        [MaxLength(255)]
+        public required string Salt { get; set; }
+        [MaxLength(50)]
+        public required string Role { get; set; }
+    }
+}
---
+++ SDDAssignmentBackend/Enums/RoleEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SDDAssignmentBackend.Enums
+{
+    public enum RoleEnum
+    {
+        [Description("Administrator")]
+        Admin = 1,
+        [Description("User")]
+        User = 2
+    }
+}
