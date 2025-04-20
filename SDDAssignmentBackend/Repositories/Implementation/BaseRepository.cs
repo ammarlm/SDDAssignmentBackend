dfb25a4222dc2a1d05c907192ec55a3999e7077d
AmmarAllahham <ammar.lm96@gmail.com>
2025-04-20 11:56:06 +0400

    Implement generic repository and user repository

---
+++ SDDAssignmentBackend/Repositories/Implementation/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDDAssignmentBackend.Entities;
+using SDDAssignmentBackend.Repositories.Interface;
+
+namespace SDDAssignmentBackend.Repositories.Implementation
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T? GetById(Guid id)
+        {
+            return _dbSet.FirstOrDefault(m=>m.Id == id);
+        }
+        public Task<T?> GetByIdAsync(Guid id)
+        {
+            return _dbSet.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public T Add(T entity)
+        {
+           _dbSet.Add(entity);
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(Guid id)
+        {
+            var entity = GetById(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public IQueryable<T> AsQuerable()
+        {
+            return _dbSet.AsQueryable();
+        }
+    }
+}
---
+++ SDDAssignmentBackend/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDDAssignmentBackend.Context;
+using SDDAssignmentBackend.Entities;
+using SDDAssignmentBackend.Repositories.Interface;
+
+namespace SDDAssignmentBackend.Repositories.Implementation
+{
+    public class UserRepository : BaseRepository<UserEntity>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Task<UserEntity?> GetByUsernameAsync(string username)
+        {
+            var query = AsQuerable();
+            return query.FirstOrDefaultAsync(x => x.Username == username);
+        }
+    }
+}
