e12cd4c2070591d1bbcd06022cb79cba994d35a1
AmmarAllahham <ammar.lm96@gmail.com>
2025-04-22 09:43:42 +0400

    Add default admin user to Users table

--- SDDAssignmentBackend/Script/UserTable.sql
+++ SDDAssignmentBackend/Script/UserTable.sql
@@ -7,3 +7,13 @@ CREATE TABLE Users (
     Role NVARCHAR(50) NOT NULL,
     CreatedAt DATETIME NOT NULL DEFAULT GETUTCDATE()
 );
+
+
+---- add default admin user
+  INSERT INTO [SDDAssignmentDB].[dbo].[Users] (id, Username, PasswordHash, Salt, Role, CreatedAt)
+  VALUES ('A3E5C4E8-B1CF-4BB7-7E3C-08DD815FAC26', 
+          'admin', 	
+          'iD4fheaBwyF3xmc2k2Uhfuafe7d2wQG9seE0E9LWrVc=',	
+          'DlGxbjMSbItwTWOKXoi7tw==',	
+          'Administrator',	
+          '2025-04-22 09:36:46.643');
